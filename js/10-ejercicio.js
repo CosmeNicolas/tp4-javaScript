7868a1eae43b64b6a7d9831322f4f1c3e80c98f4
CosmeNicolas <nicomas7590@gmail.com>
2023-11-20 18:14:04 -0300

    reinicie el ejercicio 10 en mi pc

---
+++ js/10-ejercicio.js
@@ -0,0 +1,60 @@
+/* /* 10- crear una clase aeropuerto con las propiedades nombreAeropuerto y lista de aviones, esta clase deberá contener el método agregarAvion, el cual recibirá un objeto de tipo Avión, además de un método buscarAvion el cual recibirá el nombre de un avión y devolverá información en caso de encontrarlo, si no lo encontró indicar con un mensaje.
+Por su parte los aviones tendrán las propiedades: nombre, capacidad, destino, lista de pasajeros. Los aviones tienen el método abordar el cual permite que un pasajero suba al avión solo si hay capacidad disponible en el mismo, caso contrario mostrar un mensaje que indique que el avión está lleno.
+
+Crear un objeto de tipo aeropuerto llamado "Aeropuerto Internacional", crear 3 objetos aviones con diferentes destinos. Agregar los 3 aviones al aeropuerto, buscar un avión y usar el método abordar.
+ */
+
+
+
+
+class Aeropuerto {
+    constructor(nombreAeropuerto)
+    {
+        this.nombreAeropuerto = nombreAeropuerto,
+        this.listaAviones = []
+    }
+    agregarAvion(avion){
+        this.listaAviones.push(avion)
+        return alert('Avion se agrego correctamente')
+       
+    }
+    buscaravion(avion){
+        for(let i = 0; i < this.listaAviones.length ; i++){
+            if(this.listaAviones[i] === nombreAvion){
+                document.write(`${this.listaAviones[i].nombre}`)
+            } else {
+                alert('No se Encontro el avion')
+            }
+        }
+    }
+}
+
+class Aviones extends Aeropuerto{
+    constructor(nombreAeropuerto,nombreAvion,capacidad,destino)
+    {
+        super(nombreAeropuerto);
+        this.nombreAvion = nombreAvion,
+        this.capacidad = capacidad,
+        this.destino = destino,
+        this.listapasajeros =  []
+    }
+    listarAviones(nombreAvion){
+        document.write(`<h1>Aviones </h1>`)
+        for(let i = 0; i < this.listaAviones.length; i++){
+            if(this.listaAviones[i] === nombreAvion){
+                document.write(`<ul>
+                <li>Nombre del Avion: ${this.listaAviones[i].nombreAvion}</li>
+                <li>Capacidad: ${this.listaAviones[i].capacidad}</li>
+                <li>Destino: ${this.listaAviones[i].destino}</li>
+                </ul>`)
+            }
+
+        }
+       
+    }
+}
+
+const avion1 = new Aviones("AeropuertoTuc","avion-1234",300,"Brasil")
+
+avion1.agregarAvion(avion1)
+avion1.listarAviones(avion1);
